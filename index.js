b4d898f0cc46077623987f8c9a2b3e2044fe8dfe
Viktor Smari <viktorsmari@gmail.com>
2018-08-30 19:53:13 +0200

    Dockerize project

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var redis = require("redis");
 var io = require('socket.io')();
 
-var dataQueue= redis.createClient();
-var tokenQueue= redis.createClient();
+// Use ENV var (for docker) if we have it, else use the default (undefined);
+var redis_host = process.env.REDIS_URL || "redis://127.0.0.1:6379/0";
+console.log("Connecting to redis_host: " + redis_host);
+
+var dataQueue= redis.createClient(redis_host);
+var tokenQueue= redis.createClient(redis_host);
 
 dataQueue.subscribe("data-received");
 tokenQueue.subscribe("token-received");
@@ -10,7 +14,7 @@ tokenQueue.subscribe("token-received");
 console.log('starting up');
 
 io.on('connection', function(socket){
-  
+
   console.log("client connected");
   socket.emit('welcome', { message: 'Welcome', id: socket.id });
 
