a2c2921b875c59a4ed7da70eb806e81bead2d274
Pral2a <guillem@pral2a.com>
2019-01-23 19:26:12 +0100

    Improved examples

--- example/index.js
+++ example/index.js
@@ -1,3 +1,9 @@
-require('socket.io-client')('ws://localhost:8000').on('data-received', function(data){
+var io = require('socket.io-client')('ws://localhost:8000');
+
+io.on('data-received', function(data){
+	console.log(data);
+});
+
+io.on('token-received', function(data){
 	console.log(data);
 });
