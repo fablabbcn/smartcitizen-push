630dc240a8aa5d013bb6e9977422bd7b3b4a4f05
Pral2a <guillem@pral2a.com>
2019-01-23 16:59:37 +0100

    Do not create a new connection instance for each broadcast

--- example/index.js
+++ example/index.js
@@ -1,3 +1,3 @@
-require('socket.io-client')('wss://localhost:8000').on('token-received', function(data){
+require('socket.io-client')('ws://localhost:8000').on('data-received', function(data){
 	console.log(data);
 });
